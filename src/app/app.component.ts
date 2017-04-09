0148470cfdcbf7a976439d9c929cfd21d3d37cf2
HungBui <hngbv95@gmail.com>
2017-04-09 12:46:08 +0700

    init.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/app.less';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent { }
